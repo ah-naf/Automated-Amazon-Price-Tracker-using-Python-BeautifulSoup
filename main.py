2be33d67de28bc2303d1e4493a39213a88925d8d
Ahnaf Hasan Shifat <81911439+ah-naf@users.noreply.github.com>
2021-05-28 16:21:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ params = {
 
 }
 
+EMAIL = input("Enter Your Email: ")
+PASSWORD = input("Enter Your Password: ")
+
+
 url = "https://www.amazon.com/TCL-32-720p-ROKU-Smart/dp/B088S3V3R4/ref=sr_1_2?dchild=1&field-shipping_option-bin=3242350011&pf_rd_i=16225009011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=85a9188d-dbd5-424e-9512-339a1227d37c&pf_rd_r=T56SC61TRMDZ3T89AJ80&pf_rd_s=merchandised-search-5&pf_rd_t=101&qid=1622047411&rnid=1266092011&s=electronics&sr=1-2&th=1"
 response = requests.get(url, headers=params)
 
@@ -26,10 +30,10 @@ if price < BUY_PRICE:
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        result = connection.login("ahnafhasanshifat@gmail.com", "Allah.....blessme01817711262")
+        result = connection.login(EMAIL, PASSWORD)
         
         connection.sendmail(
-            from_addr="ahnafhasanshifat@gmail.com",
-            to_addrs="ahnafhasanshifat@gmail.com",
+            from_addr=EMAIl,
+            to_addrs=EMAIL,
             msg=f"Subject: Price Alert!!\n\n{message}\n{url}"
         )
